1fa1bd255abdd02d1f02583b0eb7819e23586716
JuhLima85 <limajuh17@gmail.com>
2025-07-23 19:23:03 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/juh/producer/ProducerServicerApplication.java
@@ -0,0 +1,13 @@
+package br.com.juh.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerServicerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerServicerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juh/producer/ProducerServicerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juh.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
